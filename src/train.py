7ac0c1bda0ed841162bb30a347466abf7227bf04
nori <nori@DESKTOP-15SFOR4>
2023-08-29 14:57:58 +0900

    269230(before running)

--- src/train.py
+++ src/train.py
@@ -43,7 +43,7 @@ def run(cfg):
         import lightgbm as lgb
 
     #data = [pd.read_pickle(f"features/{f}.pkl") for f in cfg.features]
-    data = [pd.read_pickle('/mnt/c/Users/admin/OneDrive/ドキュメント/GitHub/otto2' + f"/features/{f}.pkl") for f in cfg.features]
+    data = [pd.read_pickle(str(cwd) + f"/features/{f}.pkl") for f in cfg.features]
     data = pd.concat(data, axis=1)
     target = data.loc[data["train"], "target"].astype(int)
     train = data[data["train"]].drop(columns=["train"])
@@ -60,7 +60,7 @@ def run(cfg):
     experiment_name = f"{'optuna_' if cfg.base.optuna else ''}{rand}"
     print("test:::::file:/" + hydra.utils.get_original_cwd() + "/mlruns")
     #print("file://mnt/c/Users/admin/OneDrive/ドキュメント/GitHub/otto2/src/mlruns")
-    #mlflow.set_tracking_uri("file://mnt/c/Users/admin/OneDrive/ドキュメント/GitHub/otto2/src/mlruns")
+    mlflow.set_tracking_uri('file:/' + hydra.utils.get_original_cwd() + '/mlruns')
 
     use_cols = pd.Series(train.columns)
     use_cols.to_csv("features.csv", index=False, header=False)
