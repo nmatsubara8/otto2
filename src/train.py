6b348aad6b0e43e19931d64b2dfc8075d23e983b
nori <nori@DESKTOP-15SFOR4>
2023-08-29 14:25:11 +0900

    252046(before running)

--- src/train.py
+++ src/train.py
@@ -43,7 +43,7 @@ def run(cfg):
         import lightgbm as lgb
 
     #data = [pd.read_pickle(f"features/{f}.pkl") for f in cfg.features]
-    data = [pd.read_pickle(str(cwd) + f"/features/{f}.pkl") for f in cfg.features]
+    data = [pd.read_pickle('/mnt/c/Users/admin/OneDrive/ドキュメント/GitHub/otto2' + f"/features/{f}.pkl") for f in cfg.features]
     data = pd.concat(data, axis=1)
     target = data.loc[data["train"], "target"].astype(int)
     train = data[data["train"]].drop(columns=["train"])
