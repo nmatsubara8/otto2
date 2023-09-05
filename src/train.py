b9327dcc7d6805e19d6fc280be1b9d96f8ac0253
nori <nori@DESKTOP-15SFOR4>
2023-09-05 11:37:53 +0900

    904529(before running)

--- src/train.py
+++ src/train.py
@@ -24,10 +24,7 @@ def save_log(score_dict):
     mlflow.log_artifact(".hydra/config.yaml")
     mlflow.log_artifact(".hydra/hydra.yaml")
     mlflow.log_artifact(".hydra/overrides.yaml")
-    #print("log_file:::::",f"{os.path.basename(__file__)[:-3]}.log")
     mlflow.log_artifact(f"{os.path.basename(__file__)[:-3]}.log")
-
-
     mlflow.log_artifact("features.csv")
 
 @git_commits(rand)
@@ -44,8 +41,8 @@ def run(cfg):
         import lightgbm as lgb
 
 
-    data = [pd.read_pickle(str(cwd.parent)+ f"/features/{f}.pkl") for f in cfg.features]
-    #print("cfg.features:",cfg.features)
+    data = [pd.read_pickle(cwd /+ f"../features/{f}.pkl") for f in cfg.features]
+
     data = pd.concat(data, axis=1)
     target = data.loc[data["train"], "target"].astype(int)
     train = data[data["train"]].drop(columns=["train"])
