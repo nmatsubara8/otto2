49ba232d85777122c5a1153d9b35f5b09eed0f8f
nmatsubara8 <nmatsubara8@hotmail.com>
2023-09-11 00:12:41 +0900

    104753(before running)

--- src/train.py
+++ src/train.py
@@ -105,7 +105,7 @@ def run(cfg):
             )
     #print('CWD***',cwd)
     #cwdはotto2
-    ss = pd.read_csv((cwd / "/data/sampleSubmission.csv"))
+    ss = pd.read_csv((cwd / "./data/sampleSubmission.csv"))
     ss.iloc[:, 1:] = pred
 
     file_path = cwd / f"outputs/{rand}.csv"
